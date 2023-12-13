aa9c9b69f734629caca67e0dc58dd670eebc934c
argentoff1 <mparinos56@gmail.com>
2023-12-13 17:35:16 +0300

    init

---
+++ app/src/main/java/com/parinos/fooddelivery/LoginUserActivity.kt
@@ -0,0 +1,11 @@
+package com.parinos.fooddelivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/parinos/fooddelivery/StartActivity.kt
@@ -0,0 +1,23 @@
+package com.parinos.fooddelivery
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+
+class StartActivity : AppCompatActivity() {
+    private lateinit var goLoginPage : AppCompatButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        goLoginPage = findViewById(R.id.go_user_login_btn)
+
+        goLoginPage.setOnClickListener {
+            val intent = Intent(this@StartActivity, LoginUserActivity :: class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
