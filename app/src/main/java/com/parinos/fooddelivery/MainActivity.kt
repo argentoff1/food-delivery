c7435d4c9186bc233c4973094241cd82a922c8c9
argentoff1 <mparinos56@gmail.com>
2023-12-16 16:24:41 +0300

    реализация bottomMenu и переходов с его помощью

--- app/src/main/java/com/parinos/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/parinos/fooddelivery/MainActivity.kt
@@ -2,10 +2,46 @@ package com.parinos.fooddelivery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.parinos.fooddelivery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        changeFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+                    changeFragment(HomeFragment())
+                }
+                R.id.cart -> {
+                    changeFragment(CartFragment())
+                }
+                R.id.search -> {
+                    changeFragment(SearchFragment())
+                }
+                R.id.list -> {
+                    changeFragment(ListFragment())
+                }
+                R.id.profile -> {
+                    changeFragment(ProfileFragment())
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+
+    }
+
+    fun changeFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
